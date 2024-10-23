b01b029ab2567d8498398df8a5f41c6566c6f2f5
lalwafi <lyallwafi@gmail.com>
2024-10-23 05:50:32 +0400

    wip

--- philo.c
+++ philo.c
@@ -6,26 +6,29 @@
 /*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:35:35 by lalwafi           #+#    #+#             */
-/*   Updated: 2024/10/16 17:55:41 by lalwafi          ###   ########.fr       */
+/*   Updated: 2024/10/23 00:57:22 by lalwafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_philo(t_philo *philo, char **av, int ac)
+void	init_env(t_env *env, char **av, int ac)
 {
-	// philo = malloc(sizeof(t_philo));
-	if (ac == 5 || ac == 6)
-	{
-		philo->nop = ft_atoi(av[1]);
-		philo->ttd = ft_atoi(av[2]);
-		philo->tte = ft_atoi(av[3]);
-		philo->tts = ft_atoi(av[4]);
-		if (ac == 6)
-			philo->meal_count = ft_atoi(av[5]);
-		else
-			philo->meal_count = -1;
-	}
+	int	i;
+
+	i = -1;
+	// env = malloc(sizeof(t_philo));
+	env->nop = ft_atoi(av[1]);
+	env->ttd = ft_atoi(av[2]);
+	env->tte = ft_atoi(av[3]);
+	env->tts = ft_atoi(av[4]);
+	if (ac == 6)
+		env->meal_count = ft_atoi(av[5]);
+	else
+		env->meal_count = -1;
+	env->forks = malloc(sizeof(int) * env->nop);
+	while (env->forks[++i] < env->nop)
+		env->forks[i] = 0;
 }
 
 void	check_args(char **av, int ac)
@@ -55,18 +58,25 @@ void	check_args(char **av, int ac)
 	}
 }
 
+void	create_them_threads(t_philos **philos, int nop)
+{
+	int	i;
+
+	i = nop - 1;
+	while (--i )
+}
+
 int	main(int ac, char **av)
 {
-	t_philo philo;
+	t_env env;
+	t_philos	**philos;
 	// (void)av;
 	if (ac < 5 || ac > 6)
-	{
-		printf("usage : ./philo num_of_philos ttd tte tts [meal_count]\n");
-		exit(EXIT_FAILURE);
-	}
+		(printf("%s\n", ARGERROR), exit(EXIT_FAILURE));
 	check_args(av, ac);
-	init_philo(&philo, av, ac);
-	printf("%d, %d, %d, %d, %d\n", philo.nop, philo.ttd, philo.tte, philo.tts, philo.meal_count);
+	init_env(&env, av, ac);
+	create_them_threads(philos, env.nop);
+	printf("%d, %d, %d, %d, %d\n", env.nop, env.ttd, env.tte, env.tts, env.meal_count);
 }
 
 // number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:36:07 by lalwafi           #+#    #+#             */
-/*   Updated: 2024/10/16 14:19:17 by lalwafi          ###   ########.fr       */
+/*   Updated: 2024/10/23 00:56:06 by lalwafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,31 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct philo
+# define ARGERROR "usage : ./philosophers [number_of_philosophers] [time_to_die] [time_to_eat] [time_to_sleep] (optional)[number_of_times_each_philosopher_must_eat]"
+
+typedef struct env
 {
-	int	nop; //number of philosophers
-	int	ttd;
-	int	tte;
-	int	tts;
-	int	meal_count; //how many meals each philosopher should eat
-} t_philo;
+	int		nop; //number of philosophers
+	int		ttd;
+	int		tte;
+	int		tts;
+	int		meal_count; //how many meals each philosopher should eat
+	int		*forks;
+} t_env;
+
+typedef struct philos
+{
+	int			index;
+	int			meals;
+	int			left_fork;
+	int			right_fork;
+	pthread_t	thread_id;	
+} t_philos;
+
 
 // functions
 
-void	init_philo(t_philo *philo, char **av, int ac);
+void	init_env(t_env *env, char **av, int ac);
 void	check_args(char **av, int ac);
 
 // utils
