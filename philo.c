e40455fe28b720f871e1a2e369480b83bf2a5858
lalwafi <lyallwafi@gmail.com>
2024-10-31 21:05:44 +0400

    wip

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:35:35 by lalwafi           #+#    #+#             */
-/*   Updated: 2024/10/29 03:26:08 by lalwafi          ###   ########.fr       */
+/*   Updated: 2024/10/31 17:41:02 by lalwafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	init_env(t_env *env, char **av, int ac)
 	int	i;
 
 	i = -1;
-	// env = malloc(sizeof(t_philo));
 	env->nop = ft_atoi(av[1]);
 	env->ttd = ft_atoi(av[2]);
 	env->tte = ft_atoi(av[3]);
@@ -27,7 +26,7 @@ void	init_env(t_env *env, char **av, int ac)
 	else
 		env->meal_count = -1;
 	env->forks = malloc(sizeof(int) * env->nop);
-	while (env->forks[++i] < env->nop)
+	while (++i < env->nop)
 		env->forks[i] = 0;
 }
 
@@ -74,22 +73,35 @@ void	create_them_threads(t_philos **philos, int nop)
 	}
 }
 
-void	whats_the_time( start_time)
+void	whats_the_time(t_env *env)
 {
-	
+	gettimeofday(&env->start_time, NULL);
+	// printf("%ld\n", env->start_time.tv_sec);
+	struct timeval	temp;
+	gettimeofday(&temp, NULL);
+	while (((temp.tv_usec - env->start_time.tv_usec)/1000 + \
+		(temp.tv_sec - env->start_time.tv_sec)*1000) < 700)
+	{
+		usleep(100);
+		gettimeofday(&temp, NULL);
+	}
+	printf("%ld\n", ((temp.tv_usec - env->start_time.tv_usec)/1000 + \
+		(temp.tv_sec - env->start_time.tv_sec)*1000));
 }
 
 int	main(int ac, char **av)
 {
 	t_env env;
 	t_philos	**philos;
-	// (void)av;
+	(void)philos;
 	if (ac < 5 || ac > 6)
 		(printf("%s\n", ARGERROR), exit(EXIT_FAILURE));
 	check_args(av, ac);
 	init_env(&env, av, ac);
-	create_them_threads(philos, env.nop);
+	// create_them_threads(philos, env.nop);
+	whats_the_time(&env);
 	printf("%d, %d, %d, %d, %d\n", env.nop, env.ttd, env.tte, env.tts, env.meal_count);
+	free(env.forks);
 }
 
 // number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: lalwafi <lalwafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:36:07 by lalwafi           #+#    #+#             */
-/*   Updated: 2024/10/29 09:41:45 by lalwafi          ###   ########.fr       */
+/*   Updated: 2024/10/31 17:42:17 by lalwafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ typedef struct env
 	int		ttd;
 	int		tte;
 	int		tts;
-	typedef timeval	start_time;
+	struct timeval	start_time;
 	int		meal_count; //how many meals each philosopher should eat
 	int		*forks;
 } t_env;
@@ -41,6 +41,11 @@ typedef struct philos
 	int			right_fork;
 	pthread_t	thread_id;	
 } t_philos;
+
+typedef struct forks
+{
+	int	
+};
 
 
 // functions
